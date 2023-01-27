3b793b87b6a31e7a18645e7d2ada520d13f345a1
moscow <vanhoa0908751520@gmail.com>
2023-01-27 07:01:27 +0700

    create project hufi ktx

---
+++ Areas/Admin/Controllers/DashController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KTX_Huit.Areas.Admin.Controllers
+{
+    public class DashController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
