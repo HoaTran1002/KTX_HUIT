4c45be63c462d53b21eda38d3b75033c667c4e7c
moscow <vanhoa0908751520@gmail.com>
2023-01-28 16:25:21 +0700

    edit floor controller(add permision)

--- Areas/Admin/Controllers/FloorController.cs
+++ Areas/Admin/Controllers/FloorController.cs
@@ -4,6 +4,7 @@ namespace KTX_Huit.Areas.Admin.Controllers
 {
     public class FloorController : Controller
     {
+        [Area("Admin")]
         public IActionResult Index()
         {
             return View();
