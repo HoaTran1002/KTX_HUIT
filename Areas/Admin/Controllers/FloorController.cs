13076e93712a2d0d27c990a7432e9aaa314d5af4
moscow <vanhoa0908751520@gmail.com>
2023-01-28 15:55:27 +0700

    initialize floor management function

---
+++ Areas/Admin/Controllers/FloorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KTX_Huit.Areas.Admin.Controllers
+{
+    public class FloorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
