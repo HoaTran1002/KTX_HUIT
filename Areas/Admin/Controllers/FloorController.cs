1e59819b075b1dd4b9910a8cbf65b1d904eee3a3
moscow <vanhoa0908751520@gmail.com>
2023-01-29 17:57:17 +0700

    edit layout

--- Areas/Admin/Controllers/FloorController.cs
+++ Areas/Admin/Controllers/FloorController.cs
@@ -4,8 +4,11 @@ namespace KTX_Huit.Areas.Admin.Controllers
 {
     public class FloorController : Controller
     {
+        [Area("Admin")]
         public IActionResult Index()
         {
+
+
             return View();
         }
     }
---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# KTX_HUIT
+đây là dự án quản lý ký túc xá, do sinh vien thực hiện
