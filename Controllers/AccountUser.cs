abc9c45cc5e0b0b56317ec394c18281620f38f8c
KAkhoa12 <90773513+KAkhoa12@users.noreply.github.com>
2023-01-30 16:24:57 +0700

    add login and register

---
+++ Controllers/AccountUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KTX_Huit.Controllers
+{
+    public class AccountUser : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
