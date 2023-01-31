6dec797c859e3f25f79bbe5f7a021ee2d0c83495
KAkhoa12 <90773513+KAkhoa12@users.noreply.github.com>
2023-01-31 17:33:19 +0700

    fix Login and Register

---
+++ Controllers/AccountUserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KTX_Huit.Controllers
+{
+    public class AccountUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
